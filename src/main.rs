ad30a319f185411e1ee990bceac80595ed472845
oldOmen <victor.pesci@outlook.com>
2026-05-20 09:54:25 +0200

    this works but is bootycheeks slow

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,32 @@ mod spawn;
 use bevy::ecs::world::CommandQueue;
 use bevy::prelude::*;
 use bevy::reflect::Array;
-use bevy::tasks::AsyncComputeTaskPool;
+use bevy::render::render_resource::ShaderStage::Compute;
 use bevy::window::PrimaryWindow;
 use rand::Rng;
 use bevy_screen_diagnostics::{ScreenDiagnosticsPlugin, ScreenFrameDiagnosticsPlugin};
+use bevy::tasks::{AsyncComputeTaskPool, Task};
+use std::future::Future;
+use std::pin::Pin;
 use crate::spawn::spawn_entities;
 
-#[derive(Debug)]
+#[derive(Component)]
+pub struct ForceCalculationTask(Task<Vec2>);
+
+#[derive(Debug, Clone)]
 pub enum Attractions{
     Blue(f32),
     Red(f32),
     Grey(f32)
 }
-#[derive(Component)]
+#[derive(Component, Clone, Debug)]
 pub struct Particle {
     color: usize,
     attractions: [Attractions; 3],
     range: f32,
 }
 
-#[derive(Component, Default)]
+#[derive(Component, Default, Clone)]
 pub struct Position {
     position: Vec2,
 }
@@ -34,6 +40,9 @@ pub struct Velocity{
 pub struct TotalForce {
     total_force: Vec2,
 }
+
+#[derive(Component)]
+struct ComputeTransform(Task<()>);
 
 struct Screen {
     width: f32,
@@ -49,12 +58,13 @@ fn main() {
         .add_plugins(ScreenDiagnosticsPlugin::default())
         .add_plugins(ScreenFrameDiagnosticsPlugin)
         .add_systems(Startup, (spawn_camera, spawn_entities))
-        .add_systems(Update,
-                     (game_loop,
-                      border_system,
-                      movement_system_parallelization,
-                      clear_terminal)
-        ).run();
+        .add_systems(Update, (
+             async_game_loop,
+             async_apply_forces,
+             movement_system_parallelization,
+             border_system,
+             clear_terminal
+        ).chain()).run();
 }
 
 fn spawn_camera(mut commands: Commands) {
@@ -80,8 +90,8 @@ fn calculate_total_force (distance: f32, range: f32, force: f32) -> f32 {
         0.0
     }
 }
-/*
-fn get_central_force (force: &_, distance: f32, range: f32){
+
+fn get_central_force (force: &Attractions, distance: f32, range: f32) -> f32{
     match force {
         Attractions::Blue(force) => {
             calculate_total_force(distance, range, *force)
@@ -92,64 +102,107 @@ fn get_central_force (force: &_, distance: f32, range: f32){
         Attractions::Red(force) => {
             calculate_total_force(distance, range, *force)
         }
-    };
+    }
+}
+/*
+fn game_loop (
+    commands: &mut Commands,
+    mut query: Query<(Entity, &Particle, &Position, &mut TotalForce)>
+) {
+
+    let thread_pool = AsyncComputeTaskPool::get();
+
+    let mut combinations = query.iter_combinations_mut();
+    while let Some([particle1, particle2]) = combinations.fetch_next() {
+
+        let (entity1, particle1, position1, mut total_force1) = (particle1.0, particle1.1, particle1.2, particle1.3);
+        let (entity2, particle2, position2, mut total_force2) = (particle2.0, particle2.1, particle2.2, particle2.3);
+
+        let delta: Vec2 = position1.position - position2.position;
+        let distance = delta.length_squared();
+        let direction = delta.normalize();
+
+        let task1 = thread_pool.spawn(async move {
+            if distance <= 700.0 * 700.0 {
+                let force1:&Attractions = &particle1.attractions[particle2.color];
+                let central_force1 = get_central_force(force1, distance, particle1.range);
+                total_force1.total_force += (central_force1 / distance) * direction;
+            }
+        });
+
+        let task2 = thread_pool.spawn(async move {
+            if distance <= 700.0 * 700.0 {
+                let force2:&Attractions = &particle2.attractions[particle1.color];
+                let central_force2 = get_central_force(force2, distance, particle2.range);
+                total_force2.total_force -= (central_force2 / distance) * direction;
+            }
+        });
+
+        commands.entity(entity1).insert(ComputeTransform(task1));
+        commands.entity(entity2).insert(ComputeTransform(task2));
+    }
 }
 
  */
 
 
-fn game_loop (
-    mut query: Query<(&Particle, &Position, &mut TotalForce)>
+fn async_game_loop(
+    mut commands: Commands,
+    query: Query<(Entity, &Particle, &Position), Without<ForceCalculationTask>>,  // Don't recalc entities with pending tasks
 ) {
-    let mut combinations = query.iter_combinations_mut();
-    while let Some([particle1, particle2]) = combinations.fetch_next() {
+    let thread_pool = AsyncComputeTaskPool::get();
 
-        let (particle1, position1, mut total_force1) = (particle1.0, particle1.1, particle1.2);
-        let (particle2, position2, mut total_force2) = (particle2.0, particle2.1, particle2.2);
+    // Collect all pairs first (you can't iterate mutably and spawn tasks)
+    let pairs: Vec<[(Entity, Particle, Position); 2]> = query
+        .iter_combinations()
+        .map(|[(e1, p1, pos1), (e2, p2, pos2)]| {
+            [(e1.clone(), p1.clone(), pos1.clone()), (e2.clone(), p2.clone(), pos2.clone())]
+        })
+        .collect();
 
-        let delta: Vec2 = position1.position - position2.position;
-        let distance = delta.length_squared();
+    for [particle1, particle2] in pairs {
+        let (entity1, particle1, position1) = particle1;
+        let (entity2, particle2, position2) = particle2;
 
-        if distance <= 700.0 * 700.0 {
-            let direction = delta.normalize();
+        let delta = position1.position - position2.position;
+        let distance_sq = delta.length_squared();
 
-            let force1 = &particle1.attractions[particle2.color];
-            let force2 = &particle2.attractions[particle1.color];
+        if distance_sq <= 700.0 * 700.0 && distance_sq > 0.0 {
+            let distance = distance_sq.sqrt();
+            let direction = delta / distance;
 
-            //let central_force1 = get_central_force(force1, distance, particle1.range);
+            // Spawn async task for particle 1
+            let particle1_clone = particle1.clone();
+            let task1 = thread_pool.spawn(async move {
+                let force = &particle1_clone.attractions[particle2.color];
+                let central_force = get_central_force(force, distance, particle1_clone.range);
+                (central_force / distance_sq) * direction  // Return the force vector
+            });
+            commands.entity(entity1).insert(ForceCalculationTask(task1));
 
-            let central_force1 = match force1 {
-                Attractions::Blue(force) => {
-                    calculate_total_force(distance, particle1.range, *force)
-                },
-                Attractions::Grey(force) => {
-                    calculate_total_force(distance, particle1.range, *force)
-                },
-                Attractions::Red(force) => {
-                    calculate_total_force(distance, particle1.range, *force)
-                }
-            };
-
-            let central_force2: f32 = match force2 {
-                Attractions::Blue(force) => {
-                    calculate_total_force(distance, particle2.range, *force)
-                },
-                Attractions::Grey(force) => {
-                    calculate_total_force(distance, particle2.range, *force)
-                },
-                Attractions::Red(force) => {
-                    calculate_total_force(distance, particle2.range, *force)
-                }
-
-
-            };
-
-            total_force1.total_force += (central_force1 / distance) * direction;
-            total_force2.total_force -= (central_force2 / distance) * direction;
+            // Spawn async task for particle 2
+            let particle2_clone = particle2.clone();
+            let task2 = thread_pool.spawn(async move {
+                let force = &particle2_clone.attractions[particle1.color];
+                let central_force = get_central_force(force, distance, particle2_clone.range);
+                -(central_force / distance_sq) * direction
+            });
+            commands.entity(entity2).insert(ForceCalculationTask(task2));
         }
     }
 }
 
+fn async_apply_forces(
+    mut commands: Commands,
+    mut query: Query<(Entity, &mut TotalForce, &mut ForceCalculationTask)>,
+) {
+    for (entity, mut total_force, mut task) in query.iter_mut() {
+        if let Some(force_vector) = futures_lite::future::block_on(futures_lite::future::poll_once(&mut task.0)) {
+            total_force.total_force += force_vector;
+            commands.entity(entity).remove::<ForceCalculationTask>();
+        }
+    }
+}
 
 fn movement_system_parallelization(mut query: Query<(&mut Transform, &mut Position, &mut Velocity, &mut TotalForce)>) {
     query.par_iter_mut().for_each(|(mut transform, mut position, mut velocity, mut total_force)| {
